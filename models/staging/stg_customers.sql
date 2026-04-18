5ee47f3d3b73077588a30b7fe0321bd856d37ae1
CarlosVilladiego <127449739+CaVilladiego@users.noreply.github.com>
2026-04-17 22:42:22 -0500

    FIX rows having same values

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -3,9 +3,9 @@
 
 SELECT
     id as id_cliente,
-    "name" as nombre,
+    name as nombre,
     contact as contacto,
-    "address" as direccion,
+    address as direccion,
     city as ciudad,
     postal_code as codigo_postal,
     country as pais
--- models/staging/stg_shippers.sql
+++ models/staging/stg_shippers.sql
@@ -3,6 +3,6 @@
 
 SELECT
     id as id_transporte,
-    "name" as nombre_transporte,
+    name as nombre_transporte,
     phone as telefono_transporte
 FROM  {{ source('bigquery_source','shippers')}}
